1c955add47d03f4b948b4e63e2d9e2a5e7811990
Naufal Nazheef <noval25fake@gmail.com>
2024-11-03 18:22:35 +0700

    Terbaru

--- FrondEnd_PQE/src/components/monitor/Navbar.jsx
+++ FrondEnd_PQE/src/components/monitor/Navbar.jsx
@@ -1,4 +1,3 @@
-//ICONS //
 import { LuUser, LuLayoutDashboard } from "react-icons/lu";
 import { FaBatteryFull, FaCarBattery, FaChargingStation } from "react-icons/fa";
 import { GrStorage } from "react-icons/gr";
@@ -7,36 +6,46 @@ import { useState } from "react";
 import { HiOutlineLogin } from "react-icons/hi";
 import Login from "../../Login";
 import { elements } from "chart.js";
-//ICONS //
 
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState(0);
-  const handleLinkClick = (index) => {
-    setActiveLink(index);
+  const [activeLink, setActiveLink] = useState(1); // Set nilai default sesuai ID pertama
+
+  const handleLinkClick = (id) => {
+    setActiveLink(id);
   };
 
   const NAVBAR_LINKS = [
-    { name: "All", icon: LuLayoutDashboard, path: "/Monitor" }, // Tambahkan path
-    { name: "Charging", icon: FaBatteryFull, path: "/MonitorCharging" }, // Tambahkan path
-    { name: "Testing", icon: FaChargingStation, path: "/MonitorTesting" }, // Tambahkan path
-    { name: "Storage", icon: GrStorage, path: "/MonitorStorage" }, // Tambahkan path
+    { id: 1, name: "All", icon: LuLayoutDashboard, path: "/Monitor" },
+    { id: 2, name: "Charging", icon: FaBatteryFull, path: "/MonitorCharging" },
+    {
+      id: 3,
+      name: "Testing",
+      icon: FaChargingStation,
+      path: "/MonitorTesting",
+    },
+    { id: 4, name: "Storage", icon: GrStorage, path: "/MonitorStorage" },
   ];
 
   return (
-    <div className="w-full bg-white fixed top-0 z-10 shadow-lg rounded-b-3xl">
+    <div className="w-full bg-white fixed top-0 z-10 shadow-custom-header rounded-b-3xl">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <h1 className="text-red-600 font-poppins text-4xl font-bold">Battery Monitoring</h1>
+        <h1 className="text-red-600 font-poppins text-4xl font-bold">
+          Battery Monitoring
+        </h1>
         {/* Navigation Links */}
         <ul className="flex space-x-2 md:space-x-6 text-red-600">
-          {NAVBAR_LINKS.map((link, index) => (
+          {NAVBAR_LINKS.map((link) => (
             <li
-              key={index}
+              key={link.id}
               className={`font-medium hover:bg-red-E01414 hover:text-white py-2 px-4 rounded-md ${
-                activeLink === index ? "bg-red-E01414 text-white" : ""
+                activeLink === link.id ? "bg-red-E01414 text-white" : ""
               }`}
-              onClick={() => handleLinkClick(index)}
             >
-              <Link to={link.path} className="flex items-center space-x-2">
+              <Link
+                to={link.path}
+                className="flex items-center space-x-2"
+                onClick={() => handleLinkClick(link.id)}
+              >
                 <span>{<link.icon />}</span>
                 <span className="hidden md:inline">{link.name}</span>
               </Link>
@@ -45,11 +54,11 @@ const Navbar = () => {
         </ul>
         {/* Logo */}
         <div className="flex items-center">
-          <Link to="/login"> {/* Link ke halaman utama atau yang diinginkan */}
+          <Link to="/login">
             <img
               src="/Honda_Logo.png"
               alt="logo"
-              className="w-10 md:w-20 cursor-pointer" // Menambahkan cursor pointer untuk menunjukkan bahwa itu adalah tautan
+              className="w-10 md:w-20 cursor-pointer"
             />
           </Link>
         </div>
