21134f3ef7007424556d11f8b5b1dad7ea45508d
Naufal Nazheef <noval25fake@gmail.com>
2024-11-15 23:46:54 +0700

    Dorong SideBar

--- FrondEnd_PQE/src/components/dashboard/Sidebar.jsx
+++ FrondEnd_PQE/src/components/dashboard/Sidebar.jsx
@@ -1,26 +1,64 @@
-import { LuUser, LuLayoutDashboard } from "react-icons/lu";
-import { FaBatteryFull, FaCarBattery, FaChargingStation } from "react-icons/fa";
-import { GrStorage } from "react-icons/gr";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { LuLayoutDashboard, LuUser } from "react-icons/lu";
+import { FaBatteryFull, FaChargingStation, FaCarBattery } from "react-icons/fa";
+import { GrStorage } from "react-icons/gr";
 import { HiOutlineLogin } from "react-icons/hi";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import "../../styleCss/SideBar/SideBarCss.css";
 
 const Sidebar = () => {
   const [activeLink, setActiveLink] = useState(0);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const handleLinkClick = (index) => {
     setActiveLink(index);
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   const SIDEBAR_LINKS = [
-    { id: 1, path: "/dashboard/home", name: "Dashboard", icon: LuLayoutDashboard },
+    {
+      id: 1,
+      path: "/dashboard/home",
+      name: "Dashboard",
+      icon: LuLayoutDashboard,
+    },
     { id: 2, path: "/dashboard/users", name: "Users", icon: LuUser },
     { id: 3, path: "/dashboard/battery", name: "Battery", icon: FaBatteryFull },
     { id: 4, path: "/dashboard/storage", name: "Storage", icon: GrStorage },
-    { id: 5, path: "/dashboard/chargingunit", name: "Charging Unit", icon: FaChargingStation },
-    { id: 6, path: "/dashboard/testingmachine", name: "Testing Machine", icon: FaCarBattery },
-    { id: 7, path: "/dashboard/testingmachine", name: "Testing Machine", icon: FaCarBattery },
-    { id: 8, path: "/dashboard/testingmachine", name: "Testing Machine", icon: FaCarBattery },
-    { id: 9, path: "/dashboard/testingmachine", name: "Testing Machine", icon: FaCarBattery },
+    {
+      id: 5,
+      path: "/dashboard/chargingunit",
+      name: "Charging Unit",
+      icon: FaChargingStation,
+    },
+    {
+      id: 6,
+      path: "/dashboard/testingmachine",
+      name: "Testing Unit",
+      icon: FaCarBattery,
+    },
+    {
+      id: 7,
+      path: "/dashboard/users",
+      name: "Charging Info",
+      icon: FaChargingStation,
+    },
+    {
+      id: 8,
+      path: "/dashboard/testing",
+      name: "Testing Info",
+      icon: FaCarBattery,
+    },
+    {
+      id: 9,
+      path: "/dashboard/storageinfo",
+      name: "Storage Info",
+      icon: GrStorage,
+    },
   ];
 
   return (
@@ -42,25 +80,111 @@ const Sidebar = () => {
         />
       </div>
 
-      {/* Scrollable Menu Links */}
-      <div className="mt-6 space-y-6 text-white pt-8 px-4 overflow-y-auto h-[calc(100vh-10rem)]"> {/* Set the height here */}
+      {/* Menu Links */}
+      <div className="mt-6 text-white pt-8 px-4 scrollable-menu hidden-scrollbar">
         <ul className="space-y-6">
-          {SIDEBAR_LINKS.slice(0, 7).map((link, index) => ( // Show only first 6 items
-            <li
-              key={index}
-              className={`font-medium rounded-md py-2 px-5 hover:bg-white hover:text-red-E01414 ${
-                activeLink === index
-                  ? "bg-white text-red-E01414 pr-0 rounded-r-none"
-                  : ""
-              }`}
-            >
+          {SIDEBAR_LINKS.slice(0, 3).map((link, index) => (
+            <li key={index}>
               <Link
                 to={link.path}
-                className="flex justify-center md:justify-start items-center md:space-x-6"
-                onClick={() => handleLinkClick(index)}
+                className={`flex items-center justify-center md:justify-start md:space-x-6 transition-all duration-300 ${
+                  activeLink === link.id
+                    ? "text-red-E01414 bg-white rounded-full"
+                    : "hover:bg-white hover:text-red-E01414 hover:rounded-full"
+                }`}
+                onClick={() => handleLinkClick(link.id)}
               >
-                <span>{link.icon()}</span>
-                <span className="text-sm hidden md:flex">{link.name}</span>
+                <span
+                  className={`flex items-center justify-center w-10 h-10 transition-all duration-300 ${
+                    activeLink === link.id
+                      ? "bg-white text-red-E01414 rounded-full"
+                      : "hover:bg-white hover:text-red-E01414 hover:rounded-full"
+                  }`}
+                >
+                  {link.icon()}
+                </span>
+                <span className="hidden md:flex text-sm">{link.name}</span>
+              </Link>
+            </li>
+          ))}
+
+          {/* Dropdown Menu */}
+          <li>
+            <div
+              className="flex items-center justify-center md:justify-start md:space-x-6 cursor-pointer hover:bg-white hover:text-red-600 hover:rounded-full transition-all duration-300 border-b-2 border-transparent hover:border-red-600"
+              onClick={toggleDropdown}
+            >
+              <span className="flex items-center justify-center w-10 h-10 bg-white text-red-600 rounded-full">
+                {FaCarBattery()}
+              </span>
+              <span className="hidden md:flex text-sm font-semibold">
+                Machine
+              </span>
+              <span className="hidden md:flex">
+                {isDropdownOpen ? <FiChevronUp /> : <FiChevronDown />}
+              </span>
+            </div>
+            {isDropdownOpen && (
+              <ul className="ml-8 mt-2 space-y-2 relative">
+                {/* Garis vertikal putih */}
+                <div className="absolute top-0 left-[-16px] h-full border-l-2 border-white"></div>
+
+                <div className="pt-2">
+                  {SIDEBAR_LINKS.slice(4, 6).map((link, index) => (
+                    <li key={index} className="relative">
+                      {/* Garis horizontal yang terhubung ke icon submenu */}
+                      <div className="absolute top-0 left-[-16px] h-full w-[calc(100%+16px)] border-t-2 border-white"></div>
+
+                      <Link
+                        to={link.path}
+                        className={`flex items-center justify-center md:justify-start md:space-x-6 transition-all duration-300 ${
+                          activeLink === link.id
+                            ? "text-red-600 bg-white rounded-full"
+                            : "hover:bg-white hover:text-red-600 hover:rounded-full"
+                        }`}
+                        onClick={() => handleLinkClick(link.id)}
+                      >
+                        <span
+                          className={`flex items-center justify-center w-10 h-10 transition-all duration-300 ${
+                            activeLink === link.id
+                              ? "bg-white text-red-600 rounded-full"
+                              : "hover:bg-white hover:text-red-600 hover:rounded-full"
+                          }`}
+                        >
+                          {link.icon()}
+                        </span>
+                        <span className="hidden md:flex text-sm">
+                          {link.name}
+                        </span>
+                      </Link>
+                    </li>
+                  ))}
+                </div>
+              </ul>
+            )}
+          </li>
+
+          {SIDEBAR_LINKS.slice(6, 9).map((link, index) => (
+            <li key={index}>
+              <Link
+                to={link.path}
+                className={`flex items-center justify-center md:justify-start md:space-x-6 transition-all duration-300 ${
+                  activeLink === link.id
+                    ? "text-red-E01414 bg-white rounded-full"
+                    : "hover:bg-white hover:text-red-E01414 hover:rounded-full"
+                }`}
+                onClick={() => handleLinkClick(link.id)}
+              >
+                <span
+                  className={`flex items-center justify-center w-10 h-10 transition-all duration-300 ${
+                    activeLink === link.id
+                      ? "bg-white text-red-E01414 rounded-full"
+                      : "hover:bg-white hover:text-red-E01414 hover:rounded-full"
+                  }`}
+                >
+                  {link.icon()}
+                </span>
+                <span className="hidden md:flex text-sm">{link.name}</span>
               </Link>
             </li>
           ))}
@@ -70,10 +194,7 @@ const Sidebar = () => {
       {/* Logout button at the bottom */}
       <Link to="/login">
         <div className="w-full absolute bottom-5 left-0 px-4 py-2 cursor-pointer text-center">
-          <p
-            className="flex items-center space-x-2 text-s text-white font-bold py-2 px-5 hover:bg-white rounded-full hover:text-red-E01414"
-            path="/login"
-          >
+          <p className="flex items-center space-x-2 text-s text-white font-bold py-2 px-5 hover:bg-white rounded-full hover:text-red-E01414">
             <HiOutlineLogin />
             <span className="hidden md:inline">Log Out</span>
           </p>
