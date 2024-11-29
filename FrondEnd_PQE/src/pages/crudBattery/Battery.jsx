7837fa14c454229b89e48a960bfedc8a2dffd7d7
Naufal Nazheef <noval25fake@gmail.com>
2024-11-29 10:15:29 +0700

    Push

--- FrondEnd_PQE/src/pages/crudBattery/Battery.jsx
+++ FrondEnd_PQE/src/pages/crudBattery/Battery.jsx
@@ -78,8 +78,8 @@ const Battery = () => {
     setIsUpdateOpen(true);
   };
 
-  // if (loading) return <p className="text-center">Loading...</p>;
-  // if (error) return <p className="text-center text-red-500">{error}</p>;
+  if (loading) return <p className="text-center">Loading...</p>;
+  if (error) return <p className="text-center text-red-500">{error}</p>;
 
   const itemsPerPage = 10;
   const totalPages = Math.ceil(batteries.length / itemsPerPage);
